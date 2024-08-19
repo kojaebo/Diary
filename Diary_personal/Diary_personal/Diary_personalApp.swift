09ec6973b6a6757ae6e9a25b125ab0a61a344712
kojaebo <150410280+kojaebo@users.noreply.github.com>
2024-08-19 22:11:19 +0900

    로컬 xcode 설정

---
+++ Diary_personal/Diary_personal/Diary_personalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diary_personalApp.swift
+//  Diary_personal
+//
+//  Created by 고재보 on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Diary_personalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
